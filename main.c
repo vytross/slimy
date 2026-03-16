6c62617472993dd51d5d94cd7a091662e3ea1c0c
vytross <71793587+vytross@users.noreply.github.com>
2026-03-16 04:31:01 -0400

    Newer algorithm

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 #include "slime.h"
 
 uint64_t worldseed;
@@ -10,7 +11,6 @@ uint64_t worldseed;
 int countSlimeChunks(int, int);
 
 main() {
-
 	if (INT_MAX != 0x7fffffff) return 32767; // i'm not making this compatible with whatever the fuck machine you're on
 
 	const int threshold, range;
@@ -25,26 +25,46 @@ main() {
 	printf("\nchunk range: ");
 	scanf("%d", &range);	
 
+	// generates a gigantic 2d integer array (that's secretly a bitmap in disguise) to store all the slime chunk data
 	int** region = calloc(range * 2, sizeof(int*));
 	for (int i = 0; i < range * 2; i++) region[i] = calloc((range >> 4) + 1, sizeof(int));
 	generateRegion(region, (range >> 4) + 1, range * 2, -range, -range);
 
-	for (int zCenter = -range+7; zCenter < range-7; zCenter++) {
-		for (int xCenter = -range+7; xCenter < range-7; xCenter++) {
-			int chunks = 0;
-			if (range > 500) chunks = countSlimeChunks(xCenter, zCenter); // arbitrary cutoff, mostly for testing
-			if (range <= 500) {
-				for (int x = xCenter - 7; x < xCenter + 7; x++) {
-					for (int z = zCenter - 7; z < zCenter + 7; z++) {
-						// todo make this less bad
-						if (((x - xCenter + 0.5) * (x - xCenter + 0.5)) + ((z - zCenter + 0.5) * (z - zCenter + 0.5)) <= 64
-							&& (region[z + range][(x + range) >> 5] & (1 << (31 - (x + range) % 32)))) chunks++;
+	// int clustercount = 0;
+
+	// the better algorithm (in progress). range cutoff is arbitrary but i know from testing that 500 and 501 give the same results
+	if (range <= 500) {
+		char** densityGrid = generateDensityGrid(region, (range >> 4) + 1, range * 2);
+		for (int zCenter = -range + 8; zCenter < range - 8; zCenter++) {
+			for (int xCenter = -range + 8; xCenter < range - 8; xCenter++) {
+				// it's way faster to calculate a square around each chunk than a circle. if the square doesn't even have enough don't bother
+				if (densityGrid[zCenter + range - 8][xCenter + range - 8] > threshold) {
+					int chunks = 0;
+					for (int x = xCenter - 7; x < xCenter + 7; x++) {
+						for (int z = zCenter - 7; z < zCenter + 7; z++) {
+							if (((x - xCenter + 0.5) * (x - xCenter + 0.5)) + ((z - zCenter + 0.5) * (z - zCenter + 0.5)) <= 64
+								&& (region[z + range][(x + range) >> 5] & (1 << ((x + range) % 32)))) chunks++;
+						}
+					}
+					if (chunks >= threshold) {
+						printf("%d chunks at (%d, %d)\n", chunks, xCenter * 16, zCenter * 16); // clustercount++;
 					}
 				}
 			}
-			if (chunks >= threshold) printf("%d chunks at (%d, %d)\n", chunks, xCenter * 16, zCenter * 16);
 		}
 	}
+	// the standard approach, i keep it around for time comparison. mine wins :-)
+	if (range > 500) {
+		for (int zCenter = -range + 7; zCenter < range - 7; zCenter++) {
+			for (int xCenter = -range + 7; xCenter < range - 7; xCenter++) {
+				int chunks = countSlimeChunks(xCenter, zCenter); 
+				if (chunks >= threshold) {
+					printf("%d chunks at (%d, %d)\n", chunks, xCenter * 16, zCenter * 16); // clustercount++;
+				}
+			}
+		}
+	}
+	// printf("cluster count %d\n", clustercount);
 }
 
 int countSlimeChunks(int xCenter, int zCenter) {
