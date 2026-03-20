f4dff3fb4dcfc47924930320d6372a5c10055251
vytross <71793587+vytross@users.noreply.github.com>
2026-03-20 17:24:12 -0400

    Added much faster circle filter

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ main() {
 	scanf("%d", &threshold);
 
 	printf("\nchunk range: ");
-	scanf("%d", &range);	
+	scanf("%d", &range);
 
 	// generates a gigantic 2d integer array (that's secretly a bitmap in disguise) to store all the slime chunk data
 	int** region = calloc(range * 2, sizeof(int*));
@@ -32,6 +32,9 @@ main() {
 
 	// int clustercount = 0;
 
+	clock_t start, end;
+	start = clock();
+
 	// the better algorithm (in progress). range cutoff is arbitrary but i know from testing that 500 and 501 give the same results
 	if (range <= 500) {
 		char** densityGrid = generateDensityGrid(region, (range >> 4) + 1, range * 2);
@@ -39,13 +42,7 @@ main() {
 			for (int xCenter = -range + 8; xCenter < range - 8; xCenter++) {
 				// it's way faster to calculate a square around each chunk than a circle. if the square doesn't even have enough don't bother
 				if (densityGrid[zCenter + range - 8][xCenter + range - 8] > threshold) {
-					int chunks = 0;
-					for (int x = xCenter - 7; x < xCenter + 7; x++) {
-						for (int z = zCenter - 7; z < zCenter + 7; z++) {
-							if (((x - xCenter + 0.5) * (x - xCenter + 0.5)) + ((z - zCenter + 0.5) * (z - zCenter + 0.5)) <= 64
-								&& (region[z + range][(x + range) >> 5] & (1 << ((x + range) % 32)))) chunks++;
-						}
-					}
+					int chunks = chunkCircleCount(region, xCenter, zCenter, -range, -range);
 					if (chunks >= threshold) {
 						printf("%d chunks at (%d, %d)\n", chunks, xCenter * 16, zCenter * 16); // clustercount++;
 					}
@@ -55,8 +52,8 @@ main() {
 	}
 	// the standard approach, i keep it around for time comparison. mine wins :-)
 	if (range > 500) {
-		for (int zCenter = -range + 7; zCenter < range - 7; zCenter++) {
-			for (int xCenter = -range + 7; xCenter < range - 7; xCenter++) {
+		for (int zCenter = -range + 8; zCenter < range - 8; zCenter++) {
+			for (int xCenter = -range + 8; xCenter < range - 8; xCenter++) {
 				int chunks = countSlimeChunks(xCenter, zCenter); 
 				if (chunks >= threshold) {
 					printf("%d chunks at (%d, %d)\n", chunks, xCenter * 16, zCenter * 16); // clustercount++;
@@ -64,14 +61,16 @@ main() {
 			}
 		}
 	}
+	end = clock();
+	printf("Time taken: %f", (double)(end - start) / CLOCKS_PER_SEC);
 	// printf("cluster count %d\n", clustercount);
 }
 
 int countSlimeChunks(int xCenter, int zCenter) {
 	int chunks = 0;
-	for (int x = xCenter - 7; x < xCenter + 7; x++) {
-		for (int z = zCenter - 7; z < zCenter + 7; z++) {
-			if (((x - xCenter + 0.5) * (x - xCenter + 0.5)) + ((z - zCenter + 0.5) * (z - zCenter + 0.5)) <= 64 && !checkSlimeChunk(x, z)) {
+	for (int x = xCenter - 8; x <= xCenter + 8; x++) {
+		for (int z = zCenter - 8; z < zCenter + 8; z++) {
+			if (((x - xCenter) * (x - xCenter)) + ((z - zCenter) * (z - zCenter)) <= 75 && !checkSlimeChunk(x, z)) {
 				chunks++;
 			}
 		}
