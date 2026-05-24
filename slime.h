424b251a5ef5a96fc00f4f0f5d02d3fcfb9bb7e1
vytross <71793587+vytross@users.noreply.github.com>
2026-05-24 12:02:01 -0400

    Add files via upload

--- slime.h
+++ slime.h
@@ -1,6 +1,19 @@
 #include <stdint.h>
 
+static short circleTables[25][16][16][17][17];
+
 int checkSlimeChunk(int, int);
 
-void generateRegion(int*, int, int, int, int);
+void generateRegion(int*, int, int, struct chunkPos);
 char** generateDensityGrid(int**, int, int);
+
+int countSpawningSpaces(int**, struct chunkPos, struct chunkPos, struct blockPos*, int, int*);
+int countCircleChunks(int**, struct chunkPos, struct chunkPos);
+
+struct blockPos {
+	int x, y, z;
+};
+
+struct chunkPos {
+	int x, z;
+};
